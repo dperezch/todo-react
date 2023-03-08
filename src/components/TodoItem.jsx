3fd9c021338b41d10f13f596916a34ecce6b23f0
dperezch <davarmando@gmail.com>
2023-03-08 17:44:10 -0300

    drag and drop

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,11 @@
 import CheckIcon from "./icons/IconCheck";
 import CrossIcon from "./icons/IconCross";
+import React from "react";
 
-const TodoItem = ({ todo, removeTodo, updateTodo }) => {
+const TodoItem = React.forwardRef(({ todo, removeTodo, updateTodo, ...props }, ref) => {
   const { id, title, completed } = todo;
   return (
-    <article className="flex gap-4 border-b py-4 px-4 transition-all duration-1000 dark:bg-gray-800">
+    <article {...props} ref={ref} className="flex gap-4 border-b py-4 px-4 transition-all duration-1000 dark:bg-gray-800">
       <button
         className={`h-5 w-5 flex-none rounded-full border-2 ${
           completed &&
@@ -27,6 +28,6 @@ const TodoItem = ({ todo, removeTodo, updateTodo }) => {
       </button>
     </article>
   );
-};
+});
 
 export default TodoItem;
